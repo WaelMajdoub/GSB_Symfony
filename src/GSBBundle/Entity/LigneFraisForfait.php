29e04806ef5acd9d591145a8749e0796daff37bc
MOLINARI David <molinari.d@sio.lan>
2017-03-13 09:42:15 +0100

    Edit ORM annotations ENtity

--- src/GSBBundle/Entity/LigneFraisForfait.php
+++ src/GSBBundle/Entity/LigneFraisForfait.php
@@ -3,7 +3,7 @@ namespace GSBBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity
+* @ORM\Entity(repositoryClass="GSBBundle\Repository\LigneFraisForfaitRepository") */
  */
 class LigneFraisForfait
 {
