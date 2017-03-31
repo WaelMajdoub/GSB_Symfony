e1d9fa26051402323bd92aa753cc470a1578b680
=Feronax <=>
2017-03-31 09:28:31 +0200

    Mise en place du PDF à générer

--- web/fpdf/generationFiche.php
+++ web/fpdf/generationFiche.php
@@ -10,5 +10,22 @@ class PDF extends FPDF
 $pdf = new PDF();
 $pdf->AddPage();
 $pdf->SetFont('Times','',12);
-$pdf->Cell(0,10,'Impression de la ligne numéro ',0,1);
+
+//Ajouter un logo GSB ?
+
+//Couleur (bleu) de remplissage pour la cellule suivante (titre)
+$pdf->SetFillColor(0, 204, 204);
+$pdf->Cell(0,10,'Fiche de frais n METTRE LA VARIABLE L\'ID : METTRE LE LIBELLE DE LA FICHE',0,1, 'C', true);
+
+//Saut de ligne d'une valeur de 10
+$pdf->Ln(10);
+
+$pdf->Cell(0,10,'Fiche de frais de : ' . 'METTRE LA VARIABLE DU MOIS',0,1, 'L');
+$pdf->Cell(0,10,'Modifiee pour la derniere fois le : ' . 'METTRE LA VARIABLE DE DATE MODIF',0,1, 'L');
+
+//Séparation (modifiable)
+$pdf->Cell(0,10,'',0,1, 'C', true);
+$pdf->Cell(0,10,'L\'etat de la fiche de frais est METTRE L\'ETAT',0,1, 'L');
+$pdf->Cell(0,10,'Son montant est de METTRE LE MONTANT, et le montant valide est de METTRE MONTANT VALIDE',0,1, 'L');
+
 $pdf->Output();
