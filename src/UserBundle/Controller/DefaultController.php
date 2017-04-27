5cc774c6911238007585be824ab8fd4e159e2841
David MOLINARI <david@rmolinari.net>
2017-04-27 04:13:48 +0200

    Add queries

--- src/UserBundle/Controller/DefaultController.php
+++ src/UserBundle/Controller/DefaultController.php
@@ -14,4 +14,23 @@ class DefaultController extends Controller
     {
 
     }
+
+    /**
+     * Quelques tests de queries
+     * @Route("/test", name="test")
+     */
+    public function testAction(){
+
+        $user = $this->getUser();
+
+        $em = $this->getDoctrine()->getManager();
+        $infosUser = $em->getRepository('UserBundle:User')->getInfosVisiteurs($user->getUsername(), $user->getPassword());
+
+        $em = $this->getDoctrine()->getManager();
+        $infolignesfraishorsforfait = $em->getRepository('GSBBundle:LigneFraisHorsForfait')->getLesFraisHorsForfait($user, 200101);
+
+        return $this->render('@User/test.html.twig', array(
+            'infosUser' => $infosUser,
+            'leslignesfraishorsforfait' => $infolignesfraishorsforfait));
+    }
 }
