8d58f6d014c5e4cb0b7151c8ce42e4d6de7f4057
David MOLINARI <david@rmolinari.net>
2017-04-28 09:26:56 +0200

    Add DoctrineFixtureBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new GSBBundle\GSBBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new UserBundle\UserBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
