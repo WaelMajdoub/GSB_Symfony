6366b98168af28930c0b0b62d3ab3c45d8e247de
Asshritt <chouraki.hugo@gmail.com>
2017-05-08 12:50:42 +0200

    distribution datas tableaux

--- web/js/master.js
+++ web/js/master.js
@@ -30,8 +30,39 @@ $(document).ready(function () {
                 mois: this.value}
             }).done(function (datas) {
                 console.log(datas);
+                for(aze in datas.ligneFraisForfait) {
+                    var val;
+                    switch (datas.ligneFraisForfait[aze]['id']){
+                        case 'ETP':
+                            val = '#etape';
+                            break;
+                        case 'KM':
+                            val = '#km';
+                            break;
+                        case 'NUI':
+                            val = '#nuitee';
+                            break;
+                        case 'REP':
+                            val = '#repas';
+                            break;
+                    }
+                    $(val).val(datas.ligneFraisForfait[aze]['quantite']);
+                }
+                $('#tableHF').html();
+                for(aze in datas.ligneFraisHorsForfait) {
+                    $('#tableHF').append('<tr align="center">'+
+                        '<td width="100"><input type="text" size="12" name="hfDate1" value="'+datas.ligneFraisHorsForfait[aze]['date']['date'].substring(0,10)+'"/></td>'+
+                        '<td width="220"><input type="text" size="30" name="hfLib1" value="'+datas.ligneFraisHorsForfait[aze]['libelle']+'"/></td>'+
+                        '<td width="90"><input type="text" size="10" name="hfMont1" value="'+datas.ligneFraisHorsForfait[aze]['montant']+'"/></td>'+
+                        '<td width="80">'+
+                        '<select size="3" name="hfSitu1">'+
+                        '<option value="E">Enregistré</option>'+
+                        '<option value="V">Validé</option>'+
+                        '<option value="R">Remboursé</option>'+
+                        '</select></td>'+
+                        '</tr>')
+                }
 
-                $('#txtKm').html('123');
         }).fail(function (datas) {
             console.log(datas);
         })
