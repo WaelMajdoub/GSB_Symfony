3fb7d46b5337f90391be772361a3738d39a68fd1
Asshritt <chouraki.hugo@gmail.com>
2017-05-08 13:18:51 +0200

    Modifs validFrais

--- web/js/master.js
+++ web/js/master.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    $('#visiteurSelectionne').prepend('<option disabled selected>Visiteur</option>');
     $('#visiteurSelectionne').on('change', function () {
         $.ajax({
             url: './validFrais/moisDispoParVisiteur!Ajax',
@@ -7,6 +8,9 @@ $(document).ready(function () {
             data: 'id=' + this.value
 
         }).done(function (data) {
+            $('#trF').remove();
+            $('.trHF').remove();
+            $('#selectMoisDispo').append('<option disabled selected>Date</option>');
             $.each(data.dates, function (e, da) {
                 $('#selectMoisDispo').append($('<option>', {
                         value: da.value,
@@ -15,7 +19,6 @@ $(document).ready(function () {
                 ))
 
             })
-            console.log(data)
         }).fail(function (data) {
             console.log(data);
         })
@@ -29,7 +32,19 @@ $(document).ready(function () {
                 id: $('#visiteurSelectionne').find(':selected').val(),
                 mois: this.value}
             }).done(function (datas) {
-                console.log(datas);
+                $('#trF').remove();
+                $('#tableF').append('<tr align="center" id="trF">'+
+                    '<td id="txtRepasMidi" ><input type="text" id="repas" name="repas" value=""/></td>'+
+                    '<td id="txtNuitee"><input type="text" id="nuitee" name="nuitee" value=""/></td>'+
+                    '<td id="txtEtape"><input type="text" id="etape" name="etape" value=""/></td>'+
+                    '<td id="txtKm"><input type="text" id="km" name="km" value=""/></td>'+
+                    '<td>'+
+                    '<select size="3" name="situ">'+
+                    '<option value="E">Enregistré</option>'+
+                    '<option value="V">Validé</option>'+
+                    '<option value="R">Remboursé</option>'+
+                    '</select></td>'+
+                    '</tr>')
                 for(aze in datas.ligneFraisForfait) {
                     var val;
                     switch (datas.ligneFraisForfait[aze]['id']){
@@ -48,9 +63,9 @@ $(document).ready(function () {
                     }
                     $(val).val(datas.ligneFraisForfait[aze]['quantite']);
                 }
-                $('#tableHF').html();
+                $('.trHF').remove();
                 for(aze in datas.ligneFraisHorsForfait) {
-                    $('#tableHF').append('<tr align="center">'+
+                    $('#tableHF').append('<tr align="center" class="trHF">'+
                         '<td width="100"><input type="text" size="12" name="hfDate1" value="'+datas.ligneFraisHorsForfait[aze]['date']['date'].substring(0,10)+'"/></td>'+
                         '<td width="220"><input type="text" size="30" name="hfLib1" value="'+datas.ligneFraisHorsForfait[aze]['libelle']+'"/></td>'+
                         '<td width="90"><input type="text" size="10" name="hfMont1" value="'+datas.ligneFraisHorsForfait[aze]['montant']+'"/></td>'+
