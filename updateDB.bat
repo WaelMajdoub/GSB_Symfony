44acb8f2e06fe7bd15fc221bcb1f805a480cb579
David MOLINARI <david@rmolinari.net>
2017-05-12 17:01:29 +0200

    update bat

--- updateDB.bat
+++ updateDB.bat
@@ -1,4 +1,5 @@
 php bin/console doctrine:database:drop --force
 php bin/console doctrine:database:create
 php bin/console doctrine:schema:update --force
+php bin/console doctrine:fixtures:load
 pause
